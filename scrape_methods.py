701069bc29c29ca9d10628a7c55c4c40827658d1
contactogameimpact-blip <contacto.gameimpact@gmail.com>
2026-03-11 09:17:33 -0400

    Create scrape_methods.py

---
+++ scrape_methods.py
@@ -0,0 +1,46 @@
+import json
+import requests
+from bs4 import BeautifulSoup
+
+URL = "https://oldschool.runescape.wiki/w/Money_making_guide"
+
+def scrape():
+
+    r = requests.get(URL)
+    soup = BeautifulSoup(r.text, "html.parser")
+
+    tables = soup.find_all("table", {"class": "wikitable"})
+
+    methods = []
+
+    for table in tables:
+
+        rows = table.find_all("tr")
+
+        for row in rows[1:]:
+
+            cols = row.find_all("td")
+
+            if len(cols) < 2:
+                continue
+
+            name = cols[0].get_text(strip=True)
+
+            methods.append({
+                "name": name,
+                "actions_per_hour_human": 1000,
+                "inputs": [],
+                "outputs": []
+            })
+
+    data = {
+        "methods": methods
+    }
+
+    with open("methods_base.json","w") as f:
+        json.dump(data,f,indent=2)
+
+    print("Métodos encontrados:",len(methods))
+
+if __name__ == "__main__":
+    scrape()
